9b78ebf621c6401a45ae22dd8e0f24fa3ecfa0e1
Robert Corrigan <rjcorrig@gmail.com>
2019-05-25 19:20:45 -0400

    Add storage keys to config

--- Program.cs
+++ Program.cs
@@ -1,11 +1,21 @@
 ﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
 
 namespace ToyQueue
 {
     class Program
     {
+        static IConfigurationRoot Configuration { get; set; }
         static void Main(string[] args)
         {
+            var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("storagekeys.json", optional: false, reloadOnChange: true)
+            .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+
             Console.WriteLine("Hello World!");
         }
     }
